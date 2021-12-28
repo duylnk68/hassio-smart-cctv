e50be746492fa02926ea8e3c645f3b55c95ca68d
duylnk68 <lnkduy@gmail.com>
2021-12-29 00:23:40 +0700

    TMP

--- cloudflare-argo-tunnel/entrypoint.sh
+++ cloudflare-argo-tunnel/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Environment
+CLOUDFLARE_DIR=/usr/local/etc/cloudflared
 CONFIG_PATH=/data/options.json
 url=$(jq --raw-output '.url' $CONFIG_PATH)
 AccountTag=$(jq --raw-output '.account_tag' $CONFIG_PATH)
@@ -8,18 +9,21 @@ TunnelID=$(jq --raw-output '.tunnel_id' $CONFIG_PATH)
 TunnelName=$(jq --raw-output '.tunnel_name' $CONFIG_PATH)
 TunnelSecret=$(jq --raw-output '.tunnel_secret' $CONFIG_PATH)
 
+#
+mkdir -p $CLOUDFLARE_DIR
+
 # Create credentials-file.json
 echo "{
     \"AccountTag\":\"$AccountTag\",
     \"TunnelID\":\"$TunnelID\",
     \"TunnelName\":\"$TunnelName\",
     \"TunnelSecret\":\"$TunnelSecret\"
-}" > /usr/local/etc/cloudflared/$TunnelID.json
+}" > $CLOUDFLARE_DIR/$TunnelID.json
 
 # Create config.yaml
 echo "url: $url
 tunnel: $TunnelID
-credentials-file: /usr/local/etc/cloudflared/$TunnelID.json" > /usr/local/etc/cloudflared/config.yaml
+credentials-file: $CLOUDFLARE_DIR/$TunnelID.json" > $CLOUDFLARE_DIR/config.yaml
 
 # Run
 ./cloudflared tunnel run
