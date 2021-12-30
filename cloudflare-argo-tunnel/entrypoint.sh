368684afea060cf3362aea88af74466fa3c4ab48
duylnk68 <lnkduy@gmail.com>
2021-12-30 22:03:41 +0700

    TMP

--- cloudflare-argo-tunnel/entrypoint.sh
+++ cloudflare-argo-tunnel/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Environment
-CLOUDFLARE_DIR=/usr/local/etc/cloudflared
 CONFIG_PATH=/data/options.json
 url=$(jq --raw-output '.url' $CONFIG_PATH)
 AccountTag=$(jq --raw-output '.account_tag' $CONFIG_PATH)
@@ -9,21 +8,18 @@ TunnelID=$(jq --raw-output '.tunnel_id' $CONFIG_PATH)
 TunnelName=$(jq --raw-output '.tunnel_name' $CONFIG_PATH)
 TunnelSecret=$(jq --raw-output '.tunnel_secret' $CONFIG_PATH)
 
-#
-mkdir -p $CLOUDFLARE_DIR
-
 # Create credentials-file.json
 echo "{
     \"AccountTag\":\"$AccountTag\",
     \"TunnelID\":\"$TunnelID\",
     \"TunnelName\":\"$TunnelName\",
     \"TunnelSecret\":\"$TunnelSecret\"
-}" > $CLOUDFLARE_DIR/$TunnelID.json
+}" > /data/$TunnelID.json
 
 # Create config.yaml
 echo "url: $url
 tunnel: $TunnelID
-credentials-file: $CLOUDFLARE_DIR/$TunnelID.json" > $CLOUDFLARE_DIR/config.yaml
+credentials-file: /data/$TunnelID.json" > /data/config.yaml
 
 # Run
-./cloudflared tunnel run --no-autoupdate
+./cloudflared tunnel run --no-autoupdate true --config /data/config.yaml
